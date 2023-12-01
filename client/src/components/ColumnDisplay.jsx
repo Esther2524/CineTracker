30b66821a1c804d531f7fda8892dff21f0ef58c3
Esther2524 <wangzhixiao2020@163.com>
2023-11-30 21:59:55 -0800

    Changed button styles

--- client/src/components/ColumnDisplay.jsx
+++ client/src/components/ColumnDisplay.jsx
@@ -2,18 +2,26 @@ import '../App.css';
 
 // ColumnDisplay.jsx
 const ColumnDisplay = ({ data, onMovieSelect }) => {
-    // every data is a movie object from external api
-    return (
-        <div className="column-movies-container">
-            {data.map(movie => (
-                <div key={movie.id} className="column-movie-card">
-                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="column-movie-poster" />
-                    <h2>{movie.title}</h2>
-                    <button onClick={() => onMovieSelect(movie)}>View Details</button>
-                </div>
-            ))}
+  // every data is a movie object from external api
+  return (
+    <div className="column-movies-container">
+      {data.map(movie => (
+        <div key={movie.id} className="column-movie-card">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="column-movie-poster"
+          />
+
+          <div className="movie-title">
+            <h2>{movie.title}</h2>
+          </div>
+
+          <button className='view-detail-button' onClick={() => onMovieSelect(movie)}>
+            View Details
+          </button>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 
