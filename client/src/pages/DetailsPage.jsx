b4d8c7775cb2456bbd3d102d733c70c282244be9
Esther2524 <wangzhixiao2020@163.com>
2023-11-30 21:37:20 -0800

    Completed code

--- client/src/pages/DetailsPage.jsx
+++ client/src/pages/DetailsPage.jsx
@@ -1,38 +1,74 @@
 // DetailsPage.jsx
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 import fetchMovieDetails from '../externalAPI/fetchMovieDetails';
-import MovieDetails from '../components/MovieDetails';
+import MoviePoster from '../components/MoviePoster';
+import RatingAndReview from '../components/RatingAndReview';
+import MovieInfo from '../components/MovieInfo';
 
 const DetailsPage = () => {
-    const { movieId } = useParams();
-    const [movie, setMovie] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { user, isAuthenticated, loginWithRedirect, getAccessTokenSilently } = useAuth0();
 
-    useEffect(() => {
-        const fetchDetails = async () => {
-            try {
-                const movieData = await fetchMovieDetails(movieId);
-                setMovie(movieData);
-                setIsLoading(false);
-            } catch (err) {
-                setError(err);
-                setIsLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const movieData = await fetchMovieDetails(movieId);
+        setMovie(movieData);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err);
+        setIsLoading(false);
+      }
+    };
 
-        fetchDetails();
-    }, [movieId]);
+    fetchDetails();
+  }, [movieId]);
 
-    // if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.message}</div>;
 
-    return (
-        <div>
-            {movie && <MovieDetails movie={movie} />}
+  const handleRatingAndReviewSubmit = async ({ rating, review }) => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+      return;
+    }
+    try {
+      const token = await getAccessTokenSilently();
+      await axios.post(`http://localhost:8000/api/movie/rate-and-review`, {
+        apiId: parseInt(movieId), //
+        rating: parseInt(rating),
+        review: review,
+        userEmail: user.email // I need the user's email from Auth0 and pass it to backend 
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    } catch (error) {
+      console.error('Error submitting rating and review:', error);
+    }
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className="details-page-container">
+      {movie && (
+        <div className="details-left-column">
+          <MoviePoster movie={movie} />
         </div>
-    );
+      )}
+      <div className="details-right-column">
+        {movie && <MovieInfo movie={movie} />}
+        <RatingAndReview onSubmit={handleRatingAndReviewSubmit} movieId={movieId} />
+      </div>
+    </div>
+  );
 };
 
 export default DetailsPage;
