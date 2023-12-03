ab37bec97afb339278a810c51cfe13e856598d54
Esther2524 <wangzhixiao2020@163.com>
2023-12-03 13:39:17 -0800

    Changed code

--- client/src/components/TextDisplay.jsx
+++ client/src/components/TextDisplay.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const TextDisplay = ({ movie, formatDate }) => {
     return (
-        <div key={movie.apiId} className="profile-movie-item">
-            <Link to={`/details/${movie.apiId}`} className="profile-movie-title">{movie.title}</Link>
-            <p className='profile-time'>Rated and Reviewed at {formatDate(movie.updatedAt)}</p>
-            <div className='profile-movie-rating-and-review'>
+        <div key={movie.apiId} className="text-display-movie-item">
+            <Link to={`/details/${movie.apiId}`} className="text-display-movie-title">{movie.title}</Link>
+            <p className='text-display-time'>Rated and Reviewed at {formatDate(movie.updatedAt)}</p>
+            <div className='text-display-movie-rating-and-review'>
                 <p>My Rating: {movie.rating}</p>
                 <p>My Review: {movie.review}</p>
             </div>
