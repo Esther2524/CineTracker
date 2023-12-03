accc0d58a2a16f99cde929a1e3e46d23adcca579
Esther2524 <wangzhixiao2020@163.com>
2023-12-02 17:48:57 -0800

    Changed file name

---
+++ client/src/components/CardDisplay.jsx
@@ -0,0 +1,32 @@
+import '../App.css';
+
+// CardDisplay.jsx
+const CardDisplay = ({ data, onMovieSelect }) => {
+  // every data is a movie object from external api
+  return (
+    <div className="column-movies-container">
+      {data.map(movie => (
+        <div key={movie.id} className="column-movie-card">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="column-movie-poster"
+          />
+
+          <div className="movie-title">
+            <h2>{movie.title}</h2>
+          </div>
+
+          <button className='view-detail-button' onClick={() => onMovieSelect(movie)}>
+            View Details
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+export default CardDisplay;
+
+
+
+
