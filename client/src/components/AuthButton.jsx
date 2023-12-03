5f17b2df8baaab16a11d740acbf2724546a47198
Esther2524 <wangzhixiao2020@163.com>
2023-12-03 10:45:10 -0800

    Get user name from database

--- client/src/components/AuthButton.jsx
+++ client/src/components/AuthButton.jsx
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
-export const AuthButton = () => {
-  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
+
+const fetchUserData = async (getAccessTokenSilently) => {
+  try {
+    const token = await getAccessTokenSilently();
+    const response = await axios.get('http://localhost:8000/api/user', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data; // the user data including the name
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    return null;
+  }
+};
+
+const AuthButton = () => {
+  const { user, loginWithRedirect, logout, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [isHovering, setIsHovering] = useState(false);
+  const [userName, setUserName] = useState('');
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchUserData(getAccessTokenSilently).then(userData => {
+        if (userData) {
+          setUserName(userData.name); // Set the name from the database
+        }
+      });
+    }
+  }, [isAuthenticated, getAccessTokenSilently]);
 
   return (
     <div>
       {!isAuthenticated && (
-        <span 
-          onClick={loginWithRedirect} // Directly use loginWithRedirect
-          className="login-link"
-        >
+        <span onClick={loginWithRedirect} className="login-link">
           Log In
         </span>
       )}
@@ -22,7 +47,8 @@ export const AuthButton = () => {
           onMouseLeave={() => setIsHovering(false)}
           className="user-name-container"
         >
-          <span className="user-name">{user.name}</span>
+          {/* Display the fetched name, not from Auth0. if cannot fetch name from database, then use auth0's user name */}
+          <span className="user-name">{userName || user.name}</span> 
           {isHovering && (
             <div className="dropdown-menu">
               <span 
@@ -37,4 +63,8 @@ export const AuthButton = () => {
       )}
     </div>
   );
+  
 };
+
+
+export default AuthButton;
