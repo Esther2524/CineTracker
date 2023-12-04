c0724925f02abf5f80d42289feb5bc120f9c2a90
Esther2524 <wangzhixiao2020@163.com>
2023-12-04 12:59:01 -0800

    Handle when path is null

--- client/src/components/CardDisplay.jsx
+++ client/src/components/CardDisplay.jsx
@@ -1,31 +1,38 @@
 import '../App.css';
 
 // CardDisplay.jsx
+// every data is a movie object from external api
+import '../App.css';
+
+// CardDisplay.jsx
 const CardDisplay = ({ data, onMovieSelect }) => {
-  // every data is a movie object from external api
   return (
     <div className="column-movies-container">
       {data.map(movie => (
-        <div key={movie.id} className="column-movie-card">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="column-movie-poster"
-          />
+        movie.poster_path && (
+          <div key={movie.id} className="column-movie-card">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="column-movie-poster"
+            />
 
-          <div className="movie-title">
-            <h2>{movie.title}</h2>
+            <div className="movie-title">
+              <h2>{movie.title}</h2>
+            </div>
+
+            <button className='view-detail-button' onClick={() => onMovieSelect(movie)}>
+              View Details
+            </button>
           </div>
-
-          <button className='view-detail-button' onClick={() => onMovieSelect(movie)}>
-            View Details
-          </button>
-        </div>
+        )
       ))}
     </div>
   );
 };
 
-
 export default CardDisplay;
+
 
 
 
