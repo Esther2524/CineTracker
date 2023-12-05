0dd059527bc61034c29b315c29718f4ba514e576
Esther2524 <wangzhixiao2020@163.com>
2023-12-05 15:27:40 -0800

    Changed styles

--- client/src/components/HomePageTextDisplay.jsx
+++ client/src/components/HomePageTextDisplay.jsx
@@ -7,8 +7,8 @@ const HomePageTextDisplay = ({ movie, user, formatDate }) => {
             <Link to={`/details/${movie.apiId}`} className="text-display-movie-title">{movie.title}</Link>
             <p className='text-display-time'>Posted at {formatDate(movie.updatedAt)}</p>
             <div className='text-display-movie-rating-and-review'>
-                <p>User Name: {user.name}</p> {/* Use user prop for user information */}
-                <p>Rating: {movie.rating}</p>
+                <p>User Name: <span className='test-display-user-name'>{user.name}</span></p> {/* Use user prop for user information */}
+                <p>Rating: <span className='test-display-user-name'>{movie.rating}</span></p>
                 <p>Review: {movie.review}</p>
             </div>
         </div>
